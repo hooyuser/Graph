8a2fdfefd26e0aef6271f20913ffe011be0beccd
hooyuser <hooyuser@outlook.com>
2018-09-22 00:45:48 +0800

    add new algorithm

--- Graph/Graph.h
+++ Graph/Graph.h
@@ -1,10 +1,11 @@
 #pragma once
+#include "Stack.h"
 
 typedef enum { UNDISCOVERED, DISCOVERED, VISITED } VStatus; //顶点状态
 typedef enum { UNDETERMINED, TREE, CROSS, FORWARD, BACKWARD } EType; //边在遍历树中所属的类型
 
 template <typename Tv, typename Te> //顶点类型、边类型
-class Graph  //图Graph模板类
+class Graph  //图Graph抽象类模板
 {
 private:
 	void reset()  //所有顶点、边的辅助信息复位
@@ -136,15 +137,219 @@ void Graph<Tv, Te>::DFS(int v, int& clock)  //assert: 0 <= v < n
 			break;
 		}
 	}
-	status(v) = VISITED; 
+	status(v) = VISITED;
 	fTime(v) = ++clock; //至此，当前顶点v方告访问完毕
 }
 
 template <typename Tv, typename Te> //深度优先搜索DFS算法（全图）
-void Graph<Tv, Te>::dfs(int s) { //assert: 0 <= s < n
+void Graph<Tv, Te>::dfs(int s)  //assert: 0 <= s < n
+{
 	reset(); int clock = 0; int v = s; //初始化
 	do //逐一检查所有顶点
+	{
 		if (UNDISCOVERED == status(v)) //一旦遇到尚未发现的顶点
 			DFS(v, clock); //即从该顶点出发启动一次DFS
-	while (s != (v = (++v % n))); //按序号检查，故不漏不重
+	} while (s != (v = (++v % n))); //按序号检查，故不漏不重
 }
+
+
+template <typename Tv, typename Te>
+template <typename PU> //优先级搜索（全图）
+void Graph<Tv, Te>::pfs(int s, PU prioUpdater)  //assert: 0 <= s < n
+{
+	reset();
+	int v = s; //初始化
+	do //逐一检查所有顶点
+	{
+		if (UNDISCOVERED == status(v)) //一旦遇到尚未发现的顶点
+			PFS(v, prioUpdater); //即从该顶点出发启动一次PFS
+	} while (s != (v = (++v % n))); //按序号检查，故不漏不重
+}
+
+template <typename Tv, typename Te>
+template <typename PU> //顶点类型、边类型、优先级更新器
+void Graph<Tv, Te>::PFS(int s, PU prioUpdater)  //优先级搜索（单个连通域）
+{
+	priority(s) = 0;
+	status(s) = VISITED;
+	parent(s) = -1; //初始化，起点s加至PFS树中
+	while (1)  //将下一顶点和边加至PFS树中
+	{
+		for (int w = firstNbr(s); -1 < w; w = nextNbr(s, w)) //枚举s的所有邻居w
+			prioUpdater(this, s, w); //更新顶点w的优先级及其父顶点
+		for (int shortest = INT_MAX, w = 0; w < n; w++)
+			if (UNDISCOVERED == status(w)) //从尚未加入遍历树的顶点中
+				if (shortest > priority(w)) //选出下一个
+				{
+					shortest = priority(w);
+					s = w;
+				} //优先级最高的顶点s
+		if (VISITED == status(s))
+			break; //直至所有顶点均已加入
+		status(s) = VISITED;
+		type(parent(s), s) = TREE; //将s及与其父的联边加入遍历树
+	}
+} //通过定义具体的优先级更新策略prioUpdater，即可实现不同的算法功能
+
+template <typename Tv, typename Te>
+void Graph<Tv, Te>::bcc(int s) //基于DFS的BCC分解算法
+{
+	reset(); int clock = 0; int v = s; Stack<int> S; //栈S用以记录已访问的顶点
+	do
+		if (UNDISCOVERED == status(v)) //一旦发现未发现的顶点（新连通分量）
+		{
+			BCC(v, clock, S); //即从该顶点出发启动一次BCC
+			S.pop(); //遍历返回后，弹出栈中最后一个顶点——当前连通域的起点
+		}
+	while (s != (v = (++v % n)));
+}
+
+#define hca(x) (fTime(x)) //利用此处闲置的fTime[]充当hca[]
+template <typename Tv, typename Te> //顶点类型、边类型
+void Graph<Tv, Te>::BCC(int v, int& clock, Stack<int>& S) //assert: 0 <= v < n 
+{
+	hca(v) = dTime(v) = ++clock;
+	status(v) = DISCOVERED;
+	S.push(v); //v被发现并入栈
+	for (int u = firstNbr(v); -1 < u; u = nextNbr(v, u)) //枚举v的所有邻居u
+		switch (status(u))  //并视u的状态分别处理
+		{
+		case UNDISCOVERED:
+			parent(u) = v;
+			type(v, u) = TREE;
+			BCC(u, clock, S); //从顶点u处深入
+			if (hca(u) < dTime(v)) //遍历返回后，若发现u（通过后向边）可指向v的真祖先
+				hca(v) = min(hca(v), hca(u)); //则v亦必如此
+			else
+			{ //否则，以v为关节点（u以下即是一个BCC，且其中顶点此时正集中于栈S的顶部）
+				printf("BCC rooted at %c:", vertex(v));
+				Stack<int> temp;
+				do
+				{
+					temp.push(S.pop());
+					print(vertex(temp.top()));
+				} while (v != temp.top());
+				while (!temp.empty())
+					S.push(temp.pop());
+				while (v != S.pop()); //依次弹出当前BCC中的节点，亦可根据实际需求转存至其它结构
+				S.push(v); //最后一个顶点（关节点）重新入栈——分摊不足一次
+				printf("\n");
+			}
+			break;
+		case DISCOVERED:
+			type(v, u) = BACKWARD; //标记(v, u)，并按照“越小越高”的准则
+			if (u != parent(v))
+				hca(v) = min(hca(v), dTime(u)); //更新hca[v]
+			break;
+		default: //VISITED (digraphs only)
+			type(v, u) = (dTime(v) < dTime(u)) ? FORWARD : CROSS;
+			break;
+		}
+	status(v) = VISITED; //对v的访问结束
+}
+#undef hca
+
+template <typename Tv, typename Te> //基于DFS的拓扑排序算法
+Stack<Tv>* Graph<Tv, Te>::tSort(int s) //assert: 0 <= s < n
+{
+	reset();
+	int clock = 0;
+	int v = s;
+	Stack<Tv>* S = new Stack<Tv>; //用栈记录排序顶点
+	do {
+		if (UNDISCOVERED == status(v))
+			if (!TSort(v, clock, S))  //clock并非必需
+			{
+				/*DSA*/print(S);
+				while (!S->empty()) //任一连通域（亦即整图）非DAG
+					S->pop();
+				break; //则不必继续计算，故直接返回
+			}
+	} while (s != (v = (++v % n)));
+	return S; //若输入为DAG，则S内各顶点自顶向底排序；否则（不存在拓扑排序），S空
+}
+
+template <typename Tv, typename Te> //基于DFS的拓扑排序算法（单趟）
+bool Graph<Tv, Te>::TSort(int v, int& clock, Stack<Tv>* S)
+{ //assert: 0 <= v < n
+	dTime(v) = ++clock;
+	status(v) = DISCOVERED; //发现顶点v
+	for (int u = firstNbr(v); -1 < u; u = nextNbr(v, u)) //枚举v的所有邻居u
+		switch (status(u))
+		{ //并视u的状态分别处理
+		case UNDISCOVERED:
+			parent(u) = v;
+			type(v, u) = TREE;
+			if (!TSort(u, clock, S)) //从顶点u处出发深入搜索
+				return false; //若u及其后代不能拓扑排序（则全图亦必如此），故返回并报告
+			break;
+		case DISCOVERED:
+			type(v, u) = BACKWARD; //一旦发现后向边（非DAG），则
+			return false; //不必深入，故返回并报告
+		default: //VISITED (digraphs only)
+			type(v, u) = (dTime(v) < dTime(u)) ? FORWARD : CROSS;
+			break;
+		}
+	status(v) = VISITED;
+	S->push(vertex(v)); //顶点被标记为VISITED时，随即入栈
+	return true; //v及其后代可以拓扑排序
+}
+
+template <typename Tv, typename Te> //Prim算法：无向连通图，各边表示为方向互逆、权重相等的一对边
+void Graph<Tv, Te>::prim(int s) //assert: 0 <= s < n 
+{
+	reset();
+	priority(s) = 0;
+	for (int i = 0; i < n; i++)  //共需引入n个顶点和n-1条边
+	{
+		status(s) = VISITED;
+		if (-1 != parent(s))
+		{
+			type(parent(s), s) = TREE; //引入当前的s
+		}
+		for (int j = firstNbr(s); -1 < j; j = nextNbr(s, j)) //枚举s的所有邻居j
+		{
+			if ((status(j) == UNDISCOVERED) && (priority(j) > weight(s, j))) //对邻接顶点j做松弛
+			{
+				priority(j) = weight(s, j);
+				parent(j) = s;
+			} //与Dijkstra算法唯一的不同之处
+		}
+		for (int shortest = INT_MAX, j = 0; j < n; j++) //选出下一极短跨边
+		{
+			if ((status(j) == UNDISCOVERED) && (shortest > priority(j)))
+			{
+				shortest = priority(j); s = j;
+			}
+		}
+	}
+}
+
+template <typename Tv, typename Te> //最短路径Dijkstra算法：适用于一般的有向图
+void Graph<Tv, Te>::dijkstra(int s)  //assert: 0 <= s < n
+{
+	reset();
+	priority(s) = 0;
+	for (int i = 0; i < n; i++)  //共需引入n个顶点和n-1条边
+	{
+		status(s) = VISITED;
+		if (-1 != parent(s))
+			type(parent(s), s) = TREE; //引入当前的s
+		for (int j = firstNbr(s); -1 < j; j = nextNbr(s, j)) //枚举s的所有邻居j
+		{
+			if ((status(j) == UNDISCOVERED) && (priority(j) > priority(s) + weight(s, j))) //对邻接顶点j做松弛
+			{
+				priority(j) = priority(s) + weight(s, j);
+				parent(j) = s;
+			} //与Prim算法唯一的不同之处
+		}
+		for (int shortest = INT_MAX, j = 0; j < n; j++) //选出下一最近顶点
+		{
+			if ((status(j) == UNDISCOVERED) && (shortest > priority(j)))
+			{
+				shortest = priority(j);
+				s = j;
+			}
+		}
+	}
+} //对于无向连通图，假设每一条边表示为方向互逆、权重相等的一对边
